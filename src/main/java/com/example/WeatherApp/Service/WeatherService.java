46566241ee8b038964f32c1f4decc217a123a3d5
Aidar Myrtazin <Aigarmyrtazin@mail.ru>
2024-02-21 14:35:02 +0300

    fix bags

--- src/main/java/com/example/WeatherApp/Service/WeatherService.java
+++ src/main/java/com/example/WeatherApp/Service/WeatherService.java
@@ -1,5 +1,7 @@
 package com.example.WeatherApp.Service;
 
+import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -25,6 +27,7 @@ public class WeatherService {
     public String getWeather(String city){
         String baseUrl = "http://api.openweathermap.org/geo/1.0/direct";
         String queryParams = String.format("?q=%s&appid=0fca92e2ea86bb3bdf6882f49f492a63", city);
+        String result = "";
         String fullUrl = baseUrl + queryParams;
         HttpRequest request = HttpRequest.newBuilder().uri(URI.create(fullUrl)).GET().build();
         HttpResponse<String> response = null;
@@ -35,40 +38,36 @@ public class WeatherService {
         } catch (InterruptedException e){
             e.printStackTrace();
         }
-        String jsonstring = response.body().substring(1, response.body().length() - 1);
-        JSONObject jsonObject = new JSONObject(jsonstring);
-        double lat = jsonObject.getDouble("lat");
-        double lon = jsonObject.getDouble("lon");
-        baseUrl = "https://api.openweathermap.org/data/2.5/weather";
-        queryParams = String.format("?lat=%f&lon=%f&appid=0fca92e2ea86bb3bdf6882f49f492a63", lat, lon);
-        fullUrl = baseUrl + queryParams;
-        request = HttpRequest.newBuilder().uri(URI.create(fullUrl)).GET().build();
-        response = null;
-        try{
-            response = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
-        } catch (IOException e){
-            e.printStackTrace();
-        } catch (InterruptedException e){
+        try {
+            JSONArray jsonArray = new JSONArray(response.body());
+
+            for (int i = 0; i < jsonArray.length(); i++) {
+                JSONObject jsonObject = jsonArray.getJSONObject(i);
+                if (jsonObject.getString("name").equals(city)){
+                    double lat = jsonObject.getDouble("lat");
+                    double lon = jsonObject.getDouble("lon");
+                    baseUrl = "https://api.openweathermap.org/data/2.5/weather";
+                    queryParams = String.format("?lat=%f&lon=%f&appid=0fca92e2ea86bb3bdf6882f49f492a63", lat, lon);
+                    fullUrl = baseUrl + queryParams;
+                    request = HttpRequest.newBuilder().uri(URI.create(fullUrl)).GET().build();
+                    response = null;
+                    try{
+                        response = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
+                    } catch (IOException e){
+                        e.printStackTrace();
+                    } catch (InterruptedException e){
+                        e.printStackTrace();
+                    }
+                    result = response.body();
+                }
+            }
+        } catch (JSONException e) {
             e.printStackTrace();
         }
-        return response.body();
+        if (result.isEmpty()){
+            return "Ошибка запроса";
+        }else{
+            return result;
+        }
     }
-
-    // public JSONObject getValueToPlace(String city){
-    //     String baseUrl = "http://api.openweathermap.org/geo/1.0/direct";
-    //     String queryParams = String.format("?q=%s&appid=0fca92e2ea86bb3bdf6882f49f492a63", city);
-    //     String fullUrl = baseUrl + queryParams;
-    //     HttpRequest request = HttpRequest.newBuilder().uri(URI.create(fullUrl)).GET().build();
-    //     HttpResponse<String> response = null;
-    //     try{
-    //         response = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
-    //     } catch (IOException e){
-    //         e.printStackTrace();
-    //     } catch (InterruptedException e){
-    //         e.printStackTrace();
-    //     }
-    //     String a = response.body().substring(1, response.body().length() - 1);
-    //     JSONObject jsonObject = new JSONObject(a);
-    //     return jsonObject;
-    // }
 }
